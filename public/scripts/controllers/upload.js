6ae7e5370523fad0340ecaf2bd042b6e7b15e050
rakesh <rak1994kumar@gmail.com>
2016-11-02 12:47:23 -0700

    Upload added

---
+++ public/scripts/controllers/upload.js
@@ -0,0 +1,9 @@
+/**
+ * Created by SANTOSH on 11/2/2016.
+ */
+'use strict';
+
+angular.module('sbAdminApp')
+    .controller('UploadCtrl', function($scope) {
+
+    });
---
+++ public/scripts/directives/dashboard/Upload/upload.js
@@ -0,0 +1,10 @@
+angular.module('sbAdminApp').directive('upload', ['$compile', function ($compiler) {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/dashboard/upload.tmpl.html',
+        controller: function ($scope,$http) {
+
+        }
+
+    }
+}]);
